2375c2e1d70537c5fd6fd432800ca9ac8d083bf2
Marwan <marwansaad16@gmail.com>
2022-06-28 17:51:35 +0300

    Resolved npm dependency error 4

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -4,9 +4,9 @@ import {
   ThumbUpIcon,
   VolumeOffIcon,
   XIcon,
-} from '@heroicons/react/outline'
-import { VolumeUpIcon } from '@heroicons/react/solid'
-import MuiModal from '@mui/material/Modal'
+} from "@heroicons/react/outline";
+import { VolumeUpIcon } from "@heroicons/react/solid";
+import MuiModal from "@mui/material/Modal";
 import {
   collection,
   deleteDoc,
@@ -14,74 +14,76 @@ import {
   DocumentData,
   onSnapshot,
   setDoc,
-} from 'firebase/firestore'
-import { useEffect, useState } from 'react'
-import toast, { Toaster } from 'react-hot-toast'
-import { FaPlay } from 'react-icons/fa'
-import ReactPlayer from 'react-player/lazy'
-import { useRecoilState } from 'recoil'
-import { modalState, movieState } from '../atoms/modalAtom'
-import { db } from '../firebase'
-import useAuth from '../hooks/useAuth'
-import { Element, Genre, Movie } from '../typings'
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { FaPlay } from "react-icons/fa";
+import { useRecoilState } from "recoil";
+import { modalState, movieState } from "../atoms/modalAtom";
+import { db } from "../firebase";
+import useAuth from "../hooks/useAuth";
+import { Element, Genre, Movie } from "../typings";
+
+import _ReactPlayer, { ReactPlayerProps } from 'react-player/lazy';
+const ReactPlayer = _ReactPlayer as unknown as React.FC<ReactPlayerProps>;
 
 function Modal() {
-  const [showModal, setShowModal] = useRecoilState(modalState)
-  const [movie, setMovie] = useRecoilState(movieState)
-  const [trailer, setTrailer] = useState('')
-  const [genres, setGenres] = useState<Genre[]>([])
-  const [muted, setMuted] = useState(true)
-  const { user } = useAuth()
-  const [addedToList, setAddedToList] = useState(false)
-  const [movies, setMovies] = useState<DocumentData[] | Movie[]>([])
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const [movie, setMovie] = useRecoilState(movieState);
+  const [trailer, setTrailer] = useState("");
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [muted, setMuted] = useState(true);
+  const { user } = useAuth();
+  const [addedToList, setAddedToList] = useState(false);
+  const [movies, setMovies] = useState<DocumentData[] | Movie[]>([]);
 
   const toastStyle = {
-    background: 'white',
-    color: 'black',
-    fontWeight: 'bold',
-    fontSize: '16px',
-    padding: '15px',
-    borderRadius: '9999px',
-    maxWidth: '1000px',
-  }
+    background: "white",
+    color: "black",
+    fontWeight: "bold",
+    fontSize: "16px",
+    padding: "15px",
+    borderRadius: "9999px",
+    maxWidth: "1000px",
+  };
 
   useEffect(() => {
-    if (!movie) return
+    if (!movie) return;
 
     async function fetchMovie() {
       const data = await fetch(
         `https://api.themoviedb.org/3/${
-          movie?.media_type === 'tv' ? 'tv' : 'movie'
+          movie?.media_type === "tv" ? "tv" : "movie"
         }/${movie?.id}?api_key=${
           process.env.NEXT_PUBLIC_API_KEY
         }&language=en-US&append_to_response=videos`
       )
         .then((response) => response.json())
-        .catch((err) => console.log(err.message))
+        .catch((err) => console.log(err.message));
 
       if (data?.videos) {
         const index = data.videos.results.findIndex(
-          (element: Element) => element.type === 'Trailer'
-        )
-        setTrailer(data.videos?.results[index]?.key)
+          (element: Element) => element.type === "Trailer"
+        );
+        setTrailer(data.videos?.results[index]?.key);
       }
       if (data?.genres) {
-        setGenres(data.genres)
+        setGenres(data.genres);
       }
     }
 
-    fetchMovie()
-  }, [movie])
+    fetchMovie();
+  }, [movie]);
 
   // Find all the movies in the user's list
   useEffect(() => {
     if (user) {
       return onSnapshot(
-        collection(db, 'customers', user.uid, 'myList'),
+        collection(db, "customers", user.uid, "myList"),
         (snapshot) => setMovies(snapshot.docs)
-      )
+      );
     }
-  }, [db, movie?.id])
+  }, [db, movie?.id]);
 
   // Check if the movie is already in the user's list
   useEffect(
@@ -90,13 +92,13 @@ function Modal() {
         movies.findIndex((result) => result.data().id === movie?.id) !== -1
       ),
     [movies]
-  )
+  );
 
   const handleList = async () => {
     if (addedToList) {
       await deleteDoc(
-        doc(db, 'customers', user!.uid, 'myList', movie?.id.toString()!)
-      )
+        doc(db, "customers", user!.uid, "myList", movie?.id.toString()!)
+      );
 
       toast(
         `${movie?.title || movie?.original_name} has been removed from My List`,
@@ -104,12 +106,12 @@ function Modal() {
           duration: 8000,
           style: toastStyle,
         }
-      )
+      );
     } else {
       await setDoc(
-        doc(db, 'customers', user!.uid, 'myList', movie?.id.toString()!),
+        doc(db, "customers", user!.uid, "myList", movie?.id.toString()!),
         { ...movie }
-      )
+      );
 
       toast(
         `${movie?.title || movie?.original_name} has been added to My List`,
@@ -117,15 +119,13 @@ function Modal() {
           duration: 8000,
           style: toastStyle,
         }
-      )
+      );
     }
-  }
+  };
 
   const handleClose = () => {
-    setShowModal(false)
-  }
-
-  console.log(trailer)
+    setShowModal(false);
+  };
 
   return (
     <MuiModal
@@ -139,7 +139,7 @@ function Modal() {
           onClick={handleClose}
           className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
         >
-          <XIcon className="h-6 w-6" />
+          <XIcon className="w-6 h-6" />
         </button>
 
         <div className="relative pt-[56.25%]">
@@ -147,14 +147,14 @@ function Modal() {
             url={`https://www.youtube.com/watch?v=${trailer}`}
             width="100%"
             height="100%"
-            style={{ position: 'absolute', top: '0', left: '0' }}
+            style={{ position: "absolute", top: "0", left: "0" }}
             playing
             muted={muted}
           />
-          <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
+          <div className="absolute flex items-center justify-between w-full px-10 bottom-10">
             <div className="flex space-x-2">
               <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
-                <FaPlay className="h-7 w-7 text-black" />
+                <FaPlay className="text-black h-7 w-7" />
                 Play
               </button>
 
@@ -172,9 +172,9 @@ function Modal() {
             </div>
             <button className="modalButton" onClick={() => setMuted(!muted)}>
               {muted ? (
-                <VolumeOffIcon className="h-6 w-6" />
+                <VolumeOffIcon className="w-6 h-6" />
               ) : (
-                <VolumeUpIcon className="h-6 w-6" />
+                <VolumeUpIcon className="w-6 h-6" />
               )}
             </button>
           </div>
@@ -194,12 +194,12 @@ function Modal() {
               </div>
             </div>
 
-            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+            <div className="flex flex-col font-light gap-x-10 gap-y-4 md:flex-row">
               <p className="w-5/6">{movie?.overview}</p>
               <div className="flex flex-col space-y-3 text-sm">
                 <div>
                   <span className="text-[gray]">Genres: </span>
-                  {genres.map((genre) => genre.name).join(', ')}
+                  {genres.map((genre) => genre.name).join(", ")}
                 </div>
 
                 <div>
@@ -217,7 +217,7 @@ function Modal() {
         </div>
       </>
     </MuiModal>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
